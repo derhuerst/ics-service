42cf47315b097ccf19f1cfde0b979020e200762e
Jannis R <mail@jannisr.de>
2022-07-29 21:21:04 +0200

    test script: kill background jobs ✅

--- test.sh
+++ test.sh
@@ -7,11 +7,12 @@ set -x
 node example.js &
 sleep 1
 
+# kill child processes on exit
+# https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/2173421#2173421
+trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
+
 # fetch feed
 curl 'http://localhost:3000/feed'
 code=$?
-
-# clean up
-kill $!
 
 exit $code
