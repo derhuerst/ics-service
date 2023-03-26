376b6aa6df907386afbe3d57ad79e5236ef134b5
Gregor <apriljunge@gmail.com>
2023-03-26 14:54:22 +0200

    Bump ics to v3

--- generate-ics.js
+++ generate-ics.js
@@ -56,6 +56,9 @@ const generateIcs = (title, rawEvents, feedUrl = null) => {
 		if (ev.location) {
 			ev.location = ev.location.replace(/,/g, '\\,')
 		}
+
+		ev.calName = title
+		
 		return ev
 	})
 	let {error, value: ics} = formatEvents(events)
@@ -86,7 +89,6 @@ const generateIcs = (title, rawEvents, feedUrl = null) => {
 		const endI = markerI + methodPublish.length
 		ics = [
 			ics.slice(0, endI),
-			`X-WR-CALNAME:${title}\r\n`,
 			feedUrl ? `X-ORIGINAL-URL:${feedUrl}\r\n` : '',
 			ics.slice(endI),
 		].join('')
